5ea7a60e112a728df9fe354bb79043fc84e4fd38
ZipBoerga <zip_boerga@tutamail.com>
2025-06-21 16:35:17 +0300

    fixes

--- src/main/java/com/zip_boerga/eazy_school/controller/LoginController.java
+++ src/main/java/com/zip_boerga/eazy_school/controller/LoginController.java
@@ -1,11 +1,6 @@
 package com.zip_boerga.eazy_school.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
